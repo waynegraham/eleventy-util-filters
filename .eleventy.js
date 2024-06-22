82ccfde945b74e0d11e86fba2e6c90040010da08
Wayne Graham <wgraham@clir.org>
2024-06-22 12:22:17 -0400

    Add initial filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,127 @@
-module.exports = eleventyConfig => {
-    eleventyConfig.addFilter()
+const { DateTime } = require('luxon')
+const markdownIt = require('markdown-it')
+
+module.exports = (eleventyConfig) => {
+  // @see https://11ty.rocks/eleventyjs/data-arrays/#limit-filter
+  eleventyConfig.addFilter('randomItem', (arr) => {
+    arr.sort(() => {
+      return 0.5 - Math.random()
+    })
+    return arr.slice(0, 1)
+  })
+
+  // @see https://11ty.rocks/eleventyjs/data-arrays/#limit
+  eleventyConfig.addFilter('limit', function (arr, limit) {
+    return arr.slice(0, limit)
+  })
+
+  // @see https://11ty.rocks/eleventyjs/data-arrays/#randomlimit-filter
+  eleventyConfig.addFilter('randomLimit', (arr, limit, currPage) => {
+    // Filters out current page
+    const pageArr = arr.filter((page) => page.url !== currPage)
+
+    // Randomizes remaining items
+    pageArr.sort(() => {
+      return 0.5 - Math.random()
+    })
+
+    // Returns array items up to limit
+    return pageArr.slice(0, limit)
+  })
+
+  // @see https://11ty.rocks/eleventyjs/data-arrays/#pluck-filter
+  eleventyConfig.addFilter('pluck', function (arr, selections, attr) {
+    // Assumes this is receiving a collection, hence the `data`
+    // If custom array such as from _data, update accordingly
+    return arr.filter((item) => selections.includes(item.data[attr]))
+  })
+
+  // @see https://11ty.rocks/eleventyjs/data-arrays/#pluck-filter
+  eleventyConfig.addFilter('pluckAttribute', function (arr, value, attr) {
+    return arr.filter((item) => item.data[attr] === value)
+  })
+
+  const md = new markdownIt({
+    html: true
+  })
+  // @see https://11ty.rocks/eleventyjs/content/#markdown-filter
+  eleventyConfig.addFilter('markdownify', (content) => {
+    return md.render(content)
+  })
+
+  // @see https://11ty.rocks/eleventyjs/content/#addnbsp-filter
+  eleventyConfig.addFilter('addNbsp', (str) => {
+    if (!str) {
+      return
+    }
+    let title = str.replace(/((.*)\s(.*))$/g, '$2&nbsp;$3')
+    title = title.replace(/"(.*)"/g, '\\"$1\\"')
+    return title
+  })
+
+  // @see https://11ty.rocks/eleventyjs/content/#stripfilename-filter
+  eleventyConfig.addFilter('stripFilename', (file) => {
+    return file.replace(/\.[^/.]+$/, '')
+  })
+
+  // @see https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js#L42-L45
+  eleventyConfig.addFilter('readableDate', (dateObj, format, zone) => {
+    // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+    return DateTime.fromJSDate(dateObj, { zone: zone || 'utc' }).toFormat(
+      format || 'dd LLLL yyyy'
+    )
+  })
+
+  //   @see https://11ty.rocks/eleventyjs/content/#excerpt-filter
+  eleventyConfig.addFilter('excerpt', (post) => {
+    const content = post.replace(/(<([^>]+)>)/gi, '')
+    return content.substr(0, content.lastIndexOf(' ', 200)) + '...'
+  })
+
+  // @see https://11ty.rocks/eleventyjs/dates/#postdate-filter
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
+
+  // @see https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js#L47-L50
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd')
+  })
+
+  // Get the first `n` elements of a collection.
+  //   @see https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js#L53-L62
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return []
+    }
+    if (n < 0) {
+      return array.slice(n)
+    }
+
+    return array.slice(0, n)
+  })
+
+  // Return the smallest number argument
+  //  @see  https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js#L65-L67
+  eleventyConfig.addFilter('min', (...numbers) => {
+    return Math.min.apply(null, numbers)
+  })
+
+  // Return all the tags used in a collection
+  //   @see https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js#L70-L76
+  eleventyConfig.addFilter('getAllTags', (collection) => {
+    let tagSet = new Set()
+    for (let item of collection) {
+      ;(item.data.tags || []).forEach((tag) => tagSet.add(tag))
+    }
+    return Array.from(tagSet)
+  })
+
+  //   @see https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js#L78-L80
+  eleventyConfig.addFilter('filterTagList', function filterTagList(tags) {
+    return (tags || []).filter(
+      (tag) => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1
+    )
+  })
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Eleventy Utility Filters
 
-A set of [Eleventy](https://www.11ty.dev/) filters that are useful so you don't have to find them.
+A collection of [Eleventy](https://www.11ty.dev/) filters that are useful so you don't have to find them. These are collected from all over the web.
 
 ## Setup
 
@@ -16,5 +16,84 @@ Next, add the following to the body of the `module.exports` in your [Eleventy co
 
     {{ string | filter_name }}
 
-## Filters
+# Data Filters
+
+## randomItem
+
+Return a random item from a given array.
+
+    {{ for item in collection.all | randomItem }}
+
+## limit
+
+Return a subset of an array limited to the passed number
+
+    {{ for item in collections.all | item }}
+
+## randomLimit
+
+Given a collection and a limit in addition to the current page.url, returns the requested number of items excluding the current one. This is useful for showing additional posts without the current one being repeated in the list.
+
+    {{ for item in collections.all | randomLimit(3, page.url)}}
+
+## pluck
+
+The `pluck` filter is useful if you want to return a subset of an array based on some known attribute values, such as given an array of titles.
+
+    {%- set pickedPosts %}
+    ["Title A", "Title B"]
+    {%- endset -%}
+    {% for post in collections.all | pluck(pickedPosts, 'title') %}
+
+## pluckAttribute
+
+Same as `pluck`, but for a single attribute 
+
+    // Useful for getting a subset based on a secondary data list
+    {% for category in categories %}
+    // Then pluck just items in that "category"
+    {% for post in collections.all | pluck(category, 'category') %}
+
+# Content Filters
+
+## markdownify
+
+Sometimes you need to convert a specific bit of content into Markdown. For example, if you have created content as JSON formatted custom data within _data. In that case, you maybe don't want to use pagination to create pages but instead to loop through it somewhere.
+
+    {{ data.content | markdown | safe }}
+
+## excerpt
+
+This filter expects the full post content - post-processing - which it will strip HTML tags from and then limit to approximately the first 200 characters. The function backtracks to the space prior to the 200th character to prevent splitting words.
+
+    <p>{{ post.templateContent | excerpt }}</p>
+
+## addNbsp
+
+This filter intakes a string, such as for a page title, and inserts a non-breaking space - nbsp; - between the last two words to prevent a single word dangling on the last line (called an "orphan" by typographers).
+
+    {{ title | addNbsp }}
+
+## stripFilename
+
+Useful if using a value such as layout - which returns the full filename - in an alternate scenario, such as for part of a class name as shown.
+
+    <body class="layout--{{ layout | stripFilename }}">
+
+# Date Filters
+
+## postDate
+
+    Formats date in the formath `Jun 22 2024`
+
+    {{ page.date | postDate }}
+
+
+* readableDate
+
+* htmlDateString
+* head
+* min
+* getAllTags
+* filterTagList
 
